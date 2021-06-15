9493f5105c80fa07c5fd09a578e211fc01e269e3
celia22 <celialopez22@gmail.com>
2021-06-15 16:27:25 +0200

    it 5 multiplies dish when selected multiple  times, but no update total

--- src/components/TodaysFood/TodaysFood.jsx
+++ src/components/TodaysFood/TodaysFood.jsx
@@ -2,38 +2,48 @@ import React, { Component } from 'react';
 import "./TodaysFood.css"
 import 'bulma/css/bulma.css';
 
-let result = 0;
-
 class TodaysFood extends Component {
   constructor(props){
     super(props)
     this.state = {
-      todayFoodArray: this.props.addToList
+      todayFoodArray: this.props.addToList,
+      totalCalories: 0,
     }
   }
 
 sumCalories = () => {
-const { todayFoodArray } = this.state;
- result = todayFoodArray.food.calories * todayFoodArray.quantity
+  const { todayFoodArray } = this.state;
+  let result= 0;
+  todayFoodArray.map(x => {
+    return (
+      result = x.food.calories * x.quantity
+    )    
+  })  
+  this.setState = {
+    totalCalories: result,
+  }
+  console.log("caloires ", result)
+  console.log("quantity ", todayFoodArray.quantity)
 }
 
-  render(){
-    
+  render(){    
     const { todayFoodArray } = this.state
-    console.log("todayfood", todayFoodArray)
     return(
       <div className="todays_container">    
        <h2>Today's foods</h2>
+      
       {todayFoodArray.map((item, index) => { 
+        
         return (
           <div key={index}>
           <ul>
-            <li > {item.quantity} {item.food.name} - {result} cal</li>
-           </ul>        
+            <li > <strong>{item.quantity} {item.food.name}</strong> - {item.quantity * item.food.calories}cal</li>
+           </ul>  
+               
           </div> 
         )
       })}
-          <h4>Total: 0 cals</h4>
+          <h4> <strong>Total: </strong> {this.state.totalCalories} cals</h4>  
      </div>
 
     )
